14e7af629205072958e02401d08d2911bd398042
Eduardo Tejeda <eduardotejedadelgado@gmail.com>
2023-11-11 07:09:50 -0400

    Add routes, controllers and DB connection

--- app.js
+++ app.js
@@ -1,1 +1,41 @@
-console.log('04 Store API')
+require('dotenv').config()
+//async errors
+
+const express = require('express')
+const app = express()
+
+const connectDB = require('./db/connect')
+const productsRouter = require('./routes/products')
+
+const notFoundMiddleware = require('./middleware/not-found')
+const errorMiddleware = require('./middleware/error-handler')
+
+// middleware
+app.use(express.json())
+
+// routes
+
+app.get('/', (req, res) => {
+  res.send('<h1>Store API</h1><a href="/api/v1/products">products route</a>')
+})
+
+app.use('/api/v1/products', productsRouter)
+
+// products route
+
+app.use(notFoundMiddleware)
+app.use(errorMiddleware)
+
+const port = process.env.PORT || 5000
+
+const start = async () => {
+  try {
+    //connect DB
+    await connectDB(process.env.MONGO_URI)
+    app.listen(port, console.log(`Servir is listening ot ${port}`))
+  } catch (error) {
+    
+  }
+}
+
+start()
