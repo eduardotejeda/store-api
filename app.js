337f75bfe601b8ff0154f817b27b1ad9385d112b
Eduardo Tejeda <eduardotejedadelgado@gmail.com>
2023-11-14 06:14:25 -0400

    Populate the DB with product info using populate.js

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 require('dotenv').config()
+require('express-async-errors')
 //async errors
 
 const express = require('express')
---
+++ models/product.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose')
+
+
+const productSchema = new mongoose.Schema({
+  name: {
+    type:String,
+    require:[true, 'product name must be provided']
+  },
+  price:{
+    type:Number,
+    required:[true, 'product price must be provided']
+  },
+  featured: {
+    type:Boolean,
+    default:false
+},
+rating:{
+type: Number,
+default: 4.5,
+}, 
+createAt:{
+  type:Date,
+  default: Date.now(),
+},
+company:{
+  type:String,
+  enum: {
+    values: ['ikea', 'liddy', 'caressa', 'marcos'],
+    message: '{VALUE} is not supported',
+  },
+  //enum:['ikea', 'liddy', 'caressa', 'marcos']
+},
+
+})
+
+module.exports = mongoose.model('Product', productSchema)
