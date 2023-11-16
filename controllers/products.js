eb92406b327b91a7431377831caea77cce60bef0
Eduardo Tejeda <eduardotejedadelgado@gmail.com>
2023-11-16 09:42:54 -0400

    Add regex for better query search

--- controllers/products.js
+++ controllers/products.js
@@ -1,17 +1,31 @@
 const Product = require('../models/product')
 
 const getAllProductsStatic = async (req, res) => {
-  const products = await Product.find(
-    {name: 'vase table'}
-    )
-
+  const search = 'ab'
+  const products = await Product.find({
+    name: {$regex:search, $options: 'i'},
+  })
   res.status(200).json({products, nbHits: products.length})
 }
 
 const getAllProducts = async (req, res) => {
+  const {featured, company, name} = req.query
+  const queryObject = {}
 
-  const products = await Product.find(req.query);
-  
+  if(featured) {
+    queryObject.featured = featured === 'true' ? true: false
+  }   
+  if(company) {
+    queryObject.company = company
+  }
+  if (name) {
+    queryObject.name = {$regex: name, $options: 'i'}
+  }
+
+console.log(queryObject)
+
+
+  const products = await Product.find(queryObject);  
   res.status(200).json({products, nbHits: products.length})
 }
 
