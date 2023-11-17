718ff436b0cc2e7392c8bda650183ff25b977d35
Eduardo Tejeda <eduardotejedadelgado@gmail.com>
2023-11-17 10:01:15 -0400

    Add sort

--- controllers/products.js
+++ controllers/products.js
@@ -1,15 +1,12 @@
 const Product = require('../models/product')
 
 const getAllProductsStatic = async (req, res) => {
-  const search = 'ab'
-  const products = await Product.find({
-    name: {$regex:search, $options: 'i'},
-  })
+   const products = await Product.find({}).sort('-name')
   res.status(200).json({products, nbHits: products.length})
 }
 
 const getAllProducts = async (req, res) => {
-  const {featured, company, name} = req.query
+  const {featured, company, name, sort} = req.query
   const queryObject = {}
 
   if(featured) {
@@ -22,10 +19,18 @@ const getAllProducts = async (req, res) => {
     queryObject.name = {$regex: name, $options: 'i'}
   }
 
-console.log(queryObject)
+// console.log(queryObject)
 
 
-  const products = await Product.find(queryObject);  
+  let result = Product.find(queryObject)
+  if(sort){
+    const sortList = sort.split(',').join(' ')
+    result = result.sort(sortList)
+  } else {
+    result = result.sort('createAt')
+  }
+
+  const products = await result
   res.status(200).json({products, nbHits: products.length})
 }
 
