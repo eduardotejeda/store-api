5a4c4a5dfea049932f9c8276ebc7c4094d2798df
Eduardo Tejeda <eduardotejedadelgado@gmail.com>
2023-11-18 21:31:16 -0400

    Dont remenber

--- controllers/products.js
+++ controllers/products.js
@@ -6,7 +6,7 @@ const getAllProductsStatic = async (req, res) => {
 }
 
 const getAllProducts = async (req, res) => {
-  const {featured, company, name, sort} = req.query
+  const {featured, company, name, sort, fields} = req.query
   const queryObject = {}
 
   if(featured) {
@@ -23,11 +23,17 @@ const getAllProducts = async (req, res) => {
 
 
   let result = Product.find(queryObject)
+  //sort
   if(sort){
     const sortList = sort.split(',').join(' ')
     result = result.sort(sortList)
   } else {
     result = result.sort('createAt')
+  }
+
+  if (fields) {
+    const fieldsList = fields.split(',').join(' ');
+    result = result.select(fieldsList);
   }
 
   const products = await result
